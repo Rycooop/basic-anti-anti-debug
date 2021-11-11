ebf997e519a2c620bfbde44675cc8462adffc410
Rycooop <45408021+Rycooop@users.noreply.github.com>
2021-11-11 14:31:43 -0700

    Add files via upload

---
+++ dllmain.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <Windows.h>
+#include <winternl.h>
+
+//Simple anti-anti debug, will work for games with shit anticheat that check isDebuggerPresent(), dont try and use on BE or EAC unless you have brain damage
+
+DWORD WINAPI HackThread(HMODULE hModule)
+{
+    //Create a console
+    AllocConsole();
+    FILE* f;
+    freopen_s(&f, "conout$", "w", stdout);
+    system("Color 0B");
+    std::cout << "[*] Injected..." << std::endl;
+
+    //Locate the peb and cast to a structure
+    std::cout << "[*] Locating the PEB..." << std::endl;
+    PPEB peb = (PPEB)__readfsdword(0x30);
+    //for 64 bit games
+    //PPEB peb = (PPEB)__readgsqword(0x60);
+
+    //F3 to uninject
+    while (!GetAsyncKeyState(VK_F3) &1)
+    {
+        //Continually set PEB being debugged flag to 0
+        peb->BeingDebugged = 0;
+    }
+
+    //Shut down and uninject
+    FreeConsole();
+    fclose(f);
+    FreeLibraryAndExitThread(hModule, 0);
+    return 0;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, 0));
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
